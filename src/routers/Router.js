d656b05abc3f8ab3005043a7d75a78183044785e
shair1020 <ali99shair@gmail.com>
2022-12-02 19:22:32 +0500

    create a component and attached in router

---
+++ src/routers/Router.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import ProductDetails from "../pages/ProductDetails";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import { Route, Routes } from 'react-router-dom';
+
+
+const Router = () => {
+  return <Routes>
+    <Route path='home' element={Home} />
+    <Route path='login' element={Login} />
+    <Route path='shop' element={Shop} />
+    <Route path='shop/:id' element={ProductDetails} />
+    <Route path='cart' element={Cart} />
+    <Route path='signup' element={Signup} />
+    <Route path='checkout' element={Checkout} />
+  </Routes>
+}
+
+export default Router

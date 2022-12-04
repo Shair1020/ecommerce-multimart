cf0f3053191f87cb909db27d1c61c8314ebf374d
shair1020 <ali99shair@gmail.com>
2022-12-04 21:04:49 +0500

    footer design in progress

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/eco-logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1>Multimart</h1>
+              </div>
+              <p className="footer__text mt-4">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam
+                aspernatur, neque explicabo est omnis sunt dolores voluptates
+                ducimus fuga quis.
+              </p>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__link-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__link-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__link-title">Contact</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <span>
+                    <i className="ri-map-pin-line"></i>
+                    <p>Plot 145(Shah Faisal Colony, Al Falah Society)</p>
+                  </span>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <span>
+                    <i className="ri-phone-line"></i>
+                    <p>+923463085302</p>
+                  </span>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <span>
+                    <i className="ri-mail-line"></i>
+                    <p>example@gmailcom</p>
+                  </span>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
